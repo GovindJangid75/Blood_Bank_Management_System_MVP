16ae825cad7d8e4f8185405775b716ecfb1f761a
Govind Jangid <govindjangidt@gmail.com>
2025-11-06 18:23:02 +0530

    Connect frontend to Render backend API

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://blood-bank-management-system-qxy1.onrender.com/api'
 });
 
 // Add token to requests
